a2124f42dc8b6ea8d9fd19cd0f8e1a9075cc5cee
unknown <jreidmke@gmail.com>
2020-09-17 15:57:07 -0500

    first route

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, session, render_template, redirect, flash, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+# from models import db, connect_db, {MODEL NAME}
+# from forms import {FORM NAME}
+
+app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres4@localhost/feedback'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config["SECRET_KEY"] = "abc123"
+
+# connect_db(app)
+# db.create_all()
+
+toolbar = DebugToolbarExtension(app)
+
+@app.route('/student/id')
+def show_student_detail():
+    return render_template('student-detail.html')
