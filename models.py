cd2cc64c9779192da7ef318ba3bc196769c34c6b
unknown <jreidmke@gmail.com>
2020-09-18 09:58:35 -0500

    update student model

--- models.py
+++ models.py
@@ -27,6 +27,16 @@ class Teacher(db.Model):
     title = db.Column(db.String, nullable=False)
     school_id = db.Column(db.Integer, db.ForeignKey('schools.id', ondelete='cascade'), nullable=False)
 
+class Student(db.Model):
+    """Student model"""
+
+    __tablename____ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    dob = db.Column(db.DateTime, nullable=False) # in YY, MM, DD format
+
+
 def connect_db(app):
     """Connects application to database"""
 
