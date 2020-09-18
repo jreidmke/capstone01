2ecb5e64dfc4607c8d115a4ef5ca3a4c94669684
unknown <jreidmke@gmail.com>
2020-09-18 12:58:01 -0500

    new tests written

--- test_school_models.py
+++ test_school_models.py
@@ -1,9 +1,10 @@
-import os
 from unittest import TestCase
 from models import db, School
 from app import app
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres4@localhost/iep-test'
+
+db.drop_all()
 db.create_all()
 
 class SchoolModelTestCase(TestCase):
@@ -11,13 +12,21 @@ class SchoolModelTestCase(TestCase):
 
     def setUp(self):
         """Create school"""
-        self.school = School(name='Coultrap')
+
+        self.school1 = School(name='Coultrap')
+        self.school2 = School(name="")
+
 
     def tearDown(self):
         db.session.rollback()
+        School.query.delete()
+
 
     def test_school_model(self):
-        db.session.add(self.school)
+        db.session.add(self.school1)
         db.session.commit()
-        school = School.query.get(1)
-        self.assertEqual(self.school.name, school.name)
+        school1 = School.query.get(1)
+        self.assertEqual(self.school1.name, school1.name)
+        db.session.add(self.school2)
+        school2 = School.query.get(2)
+        self.assertEqual(self.school2.name, school2.name)
