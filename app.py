916d2b7f1ea7ba6d368b9eb20d2272474d15fb4a
unknown <jreidmke@gmail.com>
2020-09-28 12:21:51 -0500

    json response coerced to correct format, ouch

--- app.py
+++ app.py
@@ -31,9 +31,33 @@ def get_standards_list(state_code):
     standards = requests.get(f'http://commonstandardsproject.com/api/v1/jurisdictions/{state_code}').json()['data']['standardSets']
     return standards
 
-def get_grade_level_standards(grade, standards):
+def get_grade_level_standard_sets(grade, standards):
+    """Returns all State Grade Level Standard Sets per grade in arguemnt
+    Organized by subject."""
+
     grade_level_standards = [standard for standard in standards if grade in standard['educationLevels']]
-    return grade_level_standards
+
+    standards_subject = [standard_subject["subject"] for standard_subject in grade_level_standards]
+
+    standard_sets_by_subject = {}
+
+    for subject in standards_subject:
+        sub_list = []
+        for obj in grade_level_standards:
+            if subject == obj["subject"]:
+                sub_list.append({obj["document"]["title"]: obj["id"]})
+
+            standard_sets_by_subject[subject] = sub_list
+
+    return standard_sets_by_subject
+
+def append_zero_convert_to_string(int):
+    """The API formats single digit grades as strings with appended 0's (ex. 02, 07, 08)"""
+    if int <= 9:
+        return ('0' + str(int))
+    else:
+        return str(int)
+
 
 def login(user):
     """Log in user."""
@@ -52,13 +76,11 @@ def logout():
 
 # Landing page.
 
-
-
 @app.route('/')
 def show_landing_page():
     states = get_state_codes()
     standards = get_standards_list(states[0]['id'])
-    grade_level_standards = get_grade_level_standards('01', standards)
+    grade_level_standards = get_grade_level_standard_sets(append_zero_convert_to_string(2), standards)
     return render_template('landing-page.html', states=states, standards=standards, grade_level_standards=grade_level_standards)
 
 # Teacher Routing. Register and Login.
